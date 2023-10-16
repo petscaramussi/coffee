94f14dcced398a6272af89afe26810774f5b13b5
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-10-16 17:56:01 -0300

    login + register + Jwt

---
+++ Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Interfaces.Authentication;
+
+namespace Application.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            // Check if user already existis
+
+            // Create user (generate unique ID)
+
+            // Create JWT token
+            Guid userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(
+                userId,
+                firstName,
+                lastName,
+                email,
+                token);
+        }
+
+        public AuthenticationResult Login(string email, string password)
+        {
+            return new AuthenticationResult(
+                Guid.NewGuid(),
+                "firstName",
+                "lastName",
+                email,
+                "token"
+                );
+        }
+    }
+}
---
+++ Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        public string GenerateToken(Guid userId, string firstName, string lastName)
+        {
+            var siginingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes("super-secret-key+size*&¨%$#@%&$$")),
+                SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+
+            };
+
+            var securityToken = new JwtSecurityToken(
+                issuer: "Coffee",
+                expires: DateTime.Now.AddDays(1),
+                claims: claims,
+                signingCredentials: siginingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
