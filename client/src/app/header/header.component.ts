7f01526e0ddfbc910c7585880ba8c438f12334f0
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-11-24 11:34:27 -0300

    client: carrinho style

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currentRoute: string = '';
+
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getCurrentRoute();
+  }
+
+  getCurrentRoute(): void {
+    this.router.events
+      .pipe()
+      .subscribe(() => {
+        this.currentRoute =  this.router.url;
+      });
+  }
+
+
 
 }
