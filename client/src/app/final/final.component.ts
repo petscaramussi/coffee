ea94543296ad482513125d72ae2357a970a87a5f
Peterson <44592787+petscaramussi@users.noreply.github.com>
2024-02-20 14:31:42 -0300

    client: tela final

--- client/src/app/final/final.component.ts
+++ client/src/app/final/final.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pedido } from '../models/pedido';
 
 @Component({
   selector: 'app-final',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./final.component.css']
 })
 export class FinalComponent {
+  canShow: boolean = false;
+  pedidos: Pedido[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getValuesFromLocalStorage();
+  }
+
+  private getLocalStorageData(key: string): Pedido[] | null {
+    const storedData = localStorage.getItem(key);
+    if (storedData) {
+      try {
+        const parsedData = JSON.parse(storedData);
+        if (Array.isArray(parsedData)) {
+          return parsedData;
+        } else {
+          console.error('Retrieved data is not an array.');
+        }
+      } catch (error) {
+        console.error('Error parsing data:', error);
+      }
+    } else {
+      console.error(`No data found in localStorage for the key: ${key}.`);
+    }
+    return null;
+  }
+
+  private updateLocalStorageData(key: string, data: Pedido[]): void {
+    localStorage.setItem(key, JSON.stringify(data));
+    this.getValuesFromLocalStorage();
+  }
+
+  getValuesFromLocalStorage(): void {
+    if (typeof localStorage !== 'undefined') {
+      const key = 'ProductsInCart';
+      const parsedData = this.getLocalStorageData(key);
+      if (parsedData) {
+        this.pedidos = parsedData;
+      }
+    } else {
+      console.error('localStorage is not supported in this browser.');
+    }
+  }
+
+
+  getTotal(): number {
+    const key = 'ProductsInCart';
+    const menuItems: Pedido[] = this.getLocalStorageData(key) || [];
+
+    const totalPrice = menuItems.reduce((total, currentItem) => {
+      return total + currentItem.price * currentItem.qtde;
+    }, 0);
+
+    return totalPrice;
+  }
 }
